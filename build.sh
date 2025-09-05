1ff4dd4442899b149291340871610a5bcfbf2032
Mujeeb <mujeb@Mujeebs-MacBook-Pro.local>
2025-09-05 18:32:24 -0500

    Docs: update build.sh to echo correct jar path (target/controlswap-1.0.jar)

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ echo "Building SpeedrunnerSwap plugin..."
 mvn clean package
 echo ""
 echo "If build was successful, the plugin jar can be found at:"
-echo "target/speedrunnerswap-2.1.jar"
+echo "target/controlswap-1.0.jar"
 echo ""
